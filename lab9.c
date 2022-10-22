4752dffda25a1590e5533d3299d1559cf774d5ac
Irina <sekti2004@yandex.ru>
2022-10-22 16:50:45 +0300

    Rewrite after review

--- lab9.c
+++ lab9.c
@@ -1,57 +1,45 @@
 #include <stdio.h>
 
-int max(int a, int b){
-    if (a > b){
-        return a;
-    }
-    return b;
+int Max(int a, int b) {
+    return (a > b) ? a : b;
 }
 
-int min(int a, int b){
-    if (a > b){
-        return b;
-    }
-    return a;
+int Min(int a, int b) {
+    return (a < b) ? a : b;
 }
 
-int NextI(int i, int j, int l, int k){
-    return (i * min(j, l) + j * min(i, l) + k * k) % 20;
+int NextI(int i, int j, int l, int k) {
+    return (i * Min(j, l) + j * Min(i, l) + k * k) % 20;
 }
 
-int NextJ(int i, int j, int l, int k){
-    return ((i % 10 + k) * (j % 10 + k) * (l % 10 - k) % 25);
+int NextJ(int i, int j, int l, int k) {
+    return (i % 10 + k) * (j % 10 + k) * (l % 10 - k) % 25;
 }
 
-int NextL(int i, int j, int l, int k){
-    return (max(min(i + j, i + l) % 25, max(i + l, j + k) % 20) + 10);
+int NextL(int i, int j, int l, int k) {
+    return Max(Min(i + j, i + l) % 25, Max(i + l, j + k) % 20) + 10;
 }
 
-int CheckInTheArea(int i, int j){
-    if (i + j + 10 <= 0 && i + j + 20 >= 0){
-        return 1;
-    }
-    else{
-        return 0;
-    }
+int CheckInTheArea(int i, int j) {
+    return i + j + 10 <= 0 && i + j + 20 >= 0;
 }
 
-int main(void){
+int main(void) {
     int i0 = -22, j0 = 14, l0 = -14, k;
     int inArea = 0;
-    for (k = 0; k <= 50; ++k){
+    for (k = 0; k <= 50; ++k) {
         int i = NextI(i0, j0, l0, k);
         int j = NextJ(i0, j0, l0, k);
         int l = NextL(i0, j0, l0, k);
         i0 = i, j0 = j, l0 = l;
-        if (CheckInTheArea (i, j)){
+        if (CheckInTheArea(i, j)) {
             inArea = 1;
             break;
         }
     }
-    if (inArea){
+    if (inArea) {
         printf("Попадание на шаге %d\n", k);
-    }
-    else {
+    } else {
         printf("Промах: k = %d, i = %d, j = %d, l = %d\n", k, i0, j0, l0);
     }
     return 0;
