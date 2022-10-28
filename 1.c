effd6f40f4ea600d671a376f3ed55764e56a76d1
ananasik558 <denchickshow@list.ru>
2022-10-28 21:03:37 +0300

    new version

--- 1.c
+++ 1.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int max(int a, int b) {
+#define I_START 10
+#define J_START 20
+#define L_START -1
+
+int Max(int a, int b) {
     return (a>b) ? a : b;
 }
 
-int min(int a, int b) {
+int Min(int a, int b) {
     return (a<b) ? a : b;
 }
 
-int sign(int a) {
+int Sign(int a) {
     if(a > 0) {
         return 1;
     }
@@ -21,15 +25,15 @@ int sign(int a) {
     }
 }
 
-int Inext(int i, int j, int k, int l) {
-    return (abs(max(i*(k+5),j*(k+6)))-abs(min(j*(k+7),l*(k+8)))) % 20;
+int INext(int i, int j, int k, int l) {
+    return (abs(Max(i*(k+5),j*(k+6)))-abs(Min(j*(k+7),l*(k+8)))) % 20;
 }
 
-int Jnext(int i, int j, int k, int l) {
-    return ((3-sign(i-j))*abs(min(min(i*j+5,i*j-3),i*j+6))) % 25 - 7;
+int JNext(int i, int j, int k, int l) {
+    return ((3-Sign(i-j))*abs(Min(Min(i*j+5,i*j-3),i*j+6))) % 25 - 7;
 }
 
-int Lnext(int i, int j, int l) {
+int LNext(int i, int j, int l) {
     return i % 10 + j % 10 + l % 10;
 }
 
@@ -37,15 +41,15 @@ int Check(int i, int j) {
     return i>=5 && i<=10 && j<=-5 && j>=-10;
 }
 
-int main(){
-int i0 = 10, j0 = 20, l0 = -1, k;
+int Lab9(int I, int J, int L){
 int in = 0;
+int k;
 for( k = 0; k<=50; k++) {
-    int i=Inext(i0, j0, k, l0);
-    int j=Jnext(i0, j0, k, l0);
-    int l=Lnext(i0, j0, l0);
-    i0 = i, j0 = j, l0 = l;
-    if(Check(i, j)) {
+    int I_NEW=INext(I, J, k, L);
+    int J_NEW=JNext(I, J, k, L);
+    int L_NEW=LNext(I, J, L);
+    I = I_NEW, J = J_NEW, L = L_NEW;
+    if(Check(I_NEW, J_NEW)) {
         in=1;
         break;
     }
@@ -53,7 +57,12 @@ for( k = 0; k<=50; k++) {
 if(in) {
     printf("Попадание на шаге k=%d\n", k);
 } else {
-    printf("Промах k=%d, i=%d, j=%d, l=%d\n", k, i0, j0, l0);
+    printf("Промах k=%d, I=%d, J=%d, L=%d\n", k, I, J, L);
 }
-return 0;
+return 1;
+}
+
+int main(){
+    Lab9(I_START, J_START, L_START);
+    return 0;
 }
