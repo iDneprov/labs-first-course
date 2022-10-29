dc2b7c1a35693a3e065a200a1def8b81fa35eb7c
MaratS2435 <93713059+MaratS2435@users.noreply.github.com>
2022-10-29 17:36:44 +0300

    Update laba_9_1.c
    Исправил

--- laba_9_1.c
+++ laba_9_1.c
@@ -1,52 +1,57 @@
 #include <stdio.h>
 
-int abs(int a){
-    if (a >= 0) return a;
-    if (a < 0) return a * -1;
+#define I -12
+#define J -22
+#define L 11
+
+int Abs(int a) {
+    return a >= 0 ? a : (a * -1);
 }
-int max(int a, int b){
-    if (a>b) return a;
-    if (b>a) return b;
-    if (a==b) return a;
+int Max(int a, int b) {
+    return a >= b ? a : b;
 }
 
-int min(int a, int b){
-    if (a>b) return b;
-    if (b>a) return a;
-    if (a==b) return a;
+int Min(int a, int b) {
+    return a <= b ? a : b;
 }
 
-int sign(int a){
-    if (a<0) return -1;
-    if (a==0) return 0;
-    if (a>0) return 1;
+int Sign(int a) {
+    if (a<0) 
+        return -1;
+    if (a==0) 
+        return 0;
+    if (a>0) 
+        return 1;
 }
 
-int CheckInArea( int x, int y){
-    if (((x >= -10) & (x <= 0)) & ((y >= 10 - abs(x)) & (y <= 10 + abs(x)))) return 1;
-    else return 0; 
+int CheckInArea( int x, int y) {
+    if (((x >= -10) & (x <= 0)) & ((y >= 10 - abs(x)) & (y <= 10 + abs(x)))) 
+        return 1;
+    else 
+        return 0; 
 }
 
-void main(){
+int main(void) {
     int j0, k, l0, i0, i, j , l;
-    i = -12;
-    j = -22;
-    l = 11;
+    i = I;
+    j = J;
+    l = L;
     k = 0;
-    for (k = 1; k < 50; ++k){
+    for (k = 1; k < 51; ++k){
         i0 = i;
         j0 = j;
         l0 = l;
-        i = max(min(i0 - j0, j0 - l0) % 20, min(i0 - l0, j0 - k) % 20) + 10;
-        j = sign(i0 - j0) * min(i0 % 20, j0 % 20) - max(abs(i0 - l0), abs(k - 20)) % 20 + 20;
+        i = Max(Min(i0 - j0, j0 - l0) % 20, Min(i0 - l0, j0 - k) % 20) + 10;
+        j = Sign(i0 - j0) * Min(i0 % 20, j0 % 20) - Max(Abs(i0 - l0), Abs(k - 20)) % 20 + 20;
         l = (i0 % 10) * (j0 % 10) + (l0 % 10);
         if  (CheckInArea(i, j)){
             printf("Success. k = %d\nFinal coordinates: i = %d   j = %d", k, i, j);
             break;
         }
     }
-    if (!CheckInArea(i, j)){
+    if (!CheckInArea(i, j)) {
         printf("Failure\nFinal coordinates: i = %d   j = %d", i, j);
     }
+    return 0;
     
 }
