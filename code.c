d729561212eca541556d697df4c30ff7b5f2fcaf
ZBelka100 <andreyzabelkin333@gmail.com>
2022-10-29 14:12:09 +0300

    Zabelkin-lab9

--- code.c
+++ code.c
@@ -5,17 +5,19 @@
 #define l_0 -5
 
 int FindNewI(int i,int j, int l,int k) {
-    new_i = (i+j+l)*(k+1)%25 - i*j*l*(k+2)%10 + 10;
+    new_i = (i + j + l) * (k + 1) % 25 - (i * j * l * (k + 2) % 10) + 10;
     return new_i;
 }
 
 int FindNewJ(int i,int j, int l,int k) {
-    new_j = (((i+j+l)*(k+3)%25)*(((i+j+l)*(k+3)%25)>=(i*j*l*(k+4)%25)))+(i*j*l*(k+4)%25)*((i*j*l*(k+4)%25)>((i+j+l)*(k+3)%25))+10;
+    new_j = (((i + j + l) * (k + 3) % 25) * (((i + j + l) * (k + 3) % 25) >= (i * j * l * (k + 4) % 25))) +
+        (i * j * l * (k + 4) % 25) * ((i * j * l * (k + 4) % 25) > ((i + j + l) * (k + 3) % 25)) + 10;
     return new_j;
 }
 
 int FindNewL(int i,int j, int l,int k) {
-    new_l = 2*((l>0)-(l<0))*((((i+j+l)*(k+5)%10) > (i*j*l*(k+6)%25))-(((i+j+l)*(k+5)%10) < (i*j*l*(k+6)%25)))*((i+j+l)*(k+5)%10 - i*j*l*(k+6)%25);
+    new_l = 2*((l > 0) - (l < 0)) * ((((i + j + l) * (k + 5) % 10) > (i * j * l * (k + 6) % 25))-
+        (((i + j + l) * (k + 5) % 10) < (i * j * l * (k + 6) % 25)))*((i + j + l) * (k + 5) % 10 - i * j * l * (k + 6) % 25);
     return new_l;
 }
 
@@ -25,11 +27,11 @@ int main(void) {
     i = i_0;
     j = j_0;
     l = l_0;
-    for (k;k<50;++k) {
+    for (; k < 50; ++k) {
         new_i = FindNewI(i,j,l,k);
         new_j = FindNewJ(i,j,l,k);
         new_l = FindNewL(i,j,l,k);
-        if ((new_i>=5) && (new_i<=15) && (new_j<=-5) && (new_j>=-15)) {
+        if ((new_i >= 5) && (new_i <= 15) && (new_j <= -5) && (new_j >= -15)) {
             flag = 1;
             printf("Number of iterations%d\n",k);
             printf("Coordinates of the end of the movement %d %d , motion parameter %d\n",i,j,l );
