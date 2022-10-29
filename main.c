03e70309c86c978b096bc79b52b22b9334ae9b7f
Югослав <115298811+Maidov@users.noreply.github.com>
2022-10-29 16:51:21 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 
-/*
-10 - символ переноса строки
-32 - пробел
-
-
-*/
-
-
-
-void main(){
+void main() {
     char a = 0;
-    while (1){
+    while (1) {
         a = getchar();
-
         if (a == EOF) {
             break;
-        } else if (a == 10) {
-            putchar(32);
+        } else if (a == '\n') {
+            putchar(' ');
             putchar(a);
-            putchar(32);
+            putchar(' ');
         } else {
             putchar(a);
         }
     }
-}   
+}
