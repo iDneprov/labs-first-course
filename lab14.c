c77e3fe0e004eb015ac997f9dd36d7ff7b018b2c
ZBelka100 <andreyzabelkin333@gmail.com>
2022-10-29 12:53:46 +0300

    Zabelkin-lab14

--- lab14.c
+++ lab14.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-enum {maxn = 100};
+#define maxn 100
 
 int main() {
   int m[maxn][maxn];
   int dir[4] = {-1,0,1,0};
   int n;
   scanf("%i",&n);
-  for (int i = 0;i != n;++i){
-    for (int j = 0;j != n;++j){
+  for (int i = 0; i != n ;++i){
+    for (int j = 0; j != n ;++j){
         scanf("%d",&m[i][j]);
     }
   }
-  
-  int l = n, p = 0, i = n,j = n-1;
+
+  int l = n, p = 0, i = n, j = n-1;
   while (l != 0) {
-    for (int k = 0;k!=l;++k) {
+    for (int k = 0; k != l ;++k) {
       i+= dir[p%4];
       j+= dir[(p+3)%4];
       printf("%d ",m[i][j]);
