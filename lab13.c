828a00b1382543f1809e23086c4da5772e8fca0f
ZBelka100 <andreyzabelkin333@gmail.com>
2022-11-02 20:09:08 +0300

    Zabelkin-lab13

--- lab13.c
+++ lab13.c
@@ -1,2 +1,40 @@
 #include <stdio.h>
-#include <ctypr
+#include <ctype.h>
+
+#define VOWELS 35797066ULL
+
+unsigned long long int CharToSet(int c) {
+    if (c < 'a' || c > 'z') {
+        return 0;
+    } else {
+        return 1ULL << (c - 'a');
+    }
+}
+
+int main() {
+    int c;
+    unsigned long long lettersSet = 0, newLettersSet = 0;
+    c = getchar();
+    while (c != EOF) {
+        if (c != ' ' || c != '\n') {
+            lettersSet = lettersSet | CharToSet(c);
+            c = getchar();
+        } else {
+            newLettersSet = lettersSet & VOWELS;
+            if (lettersSet == newLettersSet) {
+                printf("Yes!");
+                break;
+            }
+        }
+
+        c = getchar();
+    }
+
+    newLettersSet = lettersSet & VOWELS;
+    if (lettersSet == newLettersSet) {
+        printf("Yes!");
+    } else {
+        printf("No!");
+    }
+    return 0;
+}
