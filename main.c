f57ff4a459eed812259ab1275f8ae8c068ee97d2
ananasik558 <denchickshow@list.ru>
2022-11-09 22:15:32 +0300

    new version

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int k = 1, num;
+    int k = 1;
 
     while(1) {
-        num = getchar();
+        int num = getchar();
+        if(num == ' ' || num == '\n') {
+            k = 0;
+        }
         if(num == EOF) {
             break;
         }
