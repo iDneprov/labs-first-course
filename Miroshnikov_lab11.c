3be879d6278102d6d72512edea568fecfad297c4
dmitrijmrsh <115350804+dmitrijmrsh@users.noreply.github.com>
2022-11-09 23:18:21 +0300

    Add files via upload

---
+++ Miroshnikov_lab11.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define SIZE 5
+
+int main() {
+    int k, ans = 0;
+    char a = 0;
+    while (1) {
+        a = getchar();
+        if (a == EOF) {
+            break;
+        } else {
+            if ((a != ' ') && (a != ',') && (a != '\v') && (a != '\n') && (a != '\t')) {
+                k += 1;
+            } else {
+                if ((k <= SIZE) && (k != 0)) {
+                    ans += 1;
+                }
+                k = 0; 
+            }
+        }
+    }
+    printf("%d %s", ans, "\n");
+    return 0;
+}
