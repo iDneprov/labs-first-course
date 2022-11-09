4c7fe8f6ca8ab263c588d691b1e63da0c3aad62a
ananasik558 <denchickshow@list.ru>
2022-11-09 18:18:27 +0300

    Kazartsev lab 12

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int k = 1,num;
+
+    while(1) {
+        num = getchar();
+        if(num == EOF) {
+            break;
+        }
+        if(k == 2) {
+            k = 0;
+        } else {
+            putchar(num);
+        }
+        k = k + 1;
+    }
+
+    printf("\n");
+    return 0;
+}
