28df35e80e2df44214bc82dabd2a1b56e414862e
Vasiliy <heavenmaido@gmail.com>
2022-11-09 23:40:34 +0300

    13 ЛАБА 9 Вариант

--- main.c
+++ main.c
@@ -1,17 +1,59 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+#include <ctype.h>
 
-void main() {
-    char a = 0;
+unsigned long long int GetSet(int c) {
+    c = tolower(c);
+    return 1ULL << (c);
+}
+
+int GetResult(unsigned long long leftWordSet, unsigned long long rightWordSet){
+    if ((leftWordSet & ~rightWordSet) || (rightWordSet & ~leftWordSet)){
+        return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int c = 0, flag = 0, result = 0;
+    unsigned long long leftWordSet = 0, rightWordSet = 0;
     while (1) {
-        a = getchar();
-        if (a == EOF) {
+        c = getchar();
+        if (c == EOF) {
             break;
-        } else if (a == '\n') {
-            putchar(' ');
-            putchar(a);
-            putchar(' ');
+        }
+
+        if (!((c == ' ') || (c == '\n'))){
+            switch (flag)
+            {
+            case 0:
+                flag += 1;
+            case 1:
+                leftWordSet = leftWordSet | GetSet(c);
+                break;
+            case 2:
+                flag += 1;
+            case 3:
+                rightWordSet = rightWordSet | GetSet(c);
+                break;
+            default:
+                break;
+            }
         } else {
-            putchar(a);
+            if (flag % 2) {
+                flag += (flag % 2);
+            } if (flag == 4) {
+                result += GetResult(leftWordSet, rightWordSet);
+                leftWordSet = rightWordSet;
+                rightWordSet = 0;
+                flag = 2;
+            }
         }
     }
+    
+    if (result) {
+        printf("YES!!");
+    } else {
+        printf("NO T_T");
+    }
+    putchar('\n');
 }
