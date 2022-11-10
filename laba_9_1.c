9ab101ae97685e9080200288fe51ffe2542d8707
MaratS2435 <93713059+MaratS2435@users.noreply.github.com>
2022-11-10 17:19:23 +0300

    Update laba_9_1.c
    исправил, но наверно уже поздно

--- laba_9_1.c
+++ laba_9_1.c
@@ -7,6 +7,7 @@
 int Abs(int a) {
     return a >= 0 ? a : (a * -1);
 }
+
 int Max(int a, int b) {
     return a >= b ? a : b;
 }
@@ -25,7 +26,7 @@ int Sign(int a) {
 }
 
 int CheckInArea( int x, int y) {
-    if (((x >= -10) & (x <= 0)) & ((y >= 10 - abs(x)) & (y <= 10 + abs(x)))) 
+    if (((x >= -10) && (x <= 0)) && ((y >= 10 - Abs(x)) && (y <= 10 + Abs(x)))) 
         return 1;
     else 
         return 0; 
@@ -37,21 +38,26 @@ int main(void) {
     j = J;
     l = L;
     k = 0;
+    
     for (k = 1; k < 51; ++k){
         i0 = i;
         j0 = j;
         l0 = l;
+        
         i = Max(Min(i0 - j0, j0 - l0) % 20, Min(i0 - l0, j0 - k) % 20) + 10;
         j = Sign(i0 - j0) * Min(i0 % 20, j0 % 20) - Max(Abs(i0 - l0), Abs(k - 20)) % 20 + 20;
         l = (i0 % 10) * (j0 % 10) + (l0 % 10);
+        
         if  (CheckInArea(i, j)){
             printf("Success. k = %d\nFinal coordinates: i = %d   j = %d", k, i, j);
             break;
         }
     }
+    
     if (!CheckInArea(i, j)) {
         printf("Failure\nFinal coordinates: i = %d   j = %d", i, j);
     }
+    
     return 0;
     
 }
