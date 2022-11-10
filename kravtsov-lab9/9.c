24ecaf1293ac58a5b3f7d8ee599b629b4a6a2a9a
unknown <alekseygkravtsov@yandex.ru>
2022-11-10 14:24:26 +0300

    v2.0

--- kravtsov-lab9/9.c
+++ kravtsov-lab9/9.c
@@ -28,49 +28,52 @@ int Sign(int num) {
 }
 
 
-int CheckInArea(int i,int j) {
+int CheckInArea(int i, int j) {
     
-    int firstDistance = (i - I_CENTER_FIRST)*(i - I_CENTER_FIRST) 
-    + (j - J_CENTER_FIRST)*(j - J_CENTER_FIRST);
+    int firstDistance = (i - I_CENTER_FIRST) * (i - I_CENTER_FIRST) 
+    + (j - J_CENTER_FIRST) * (j - J_CENTER_FIRST);
     
-    int secondDistance = (i - I_CENTER_SECOND)*(i - I_CENTER_SECOND) 
-    + (j - J_CENTER_SECOND)*(j - J_CENTER_SECOND);
+    int secondDistance = (i - I_CENTER_SECOND) * (i - I_CENTER_SECOND) 
+    + (j - J_CENTER_SECOND) * (j - J_CENTER_SECOND);
     
-    return (firstDistance <= RADIUS*RADIUS) && (secondDistance <= RADIUS*RADIUS);
+    return (firstDistance <= RADIUS * RADIUS) && (secondDistance <= RADIUS * RADIUS);
 }
 
 
 int IncreaseI(int k, int i, int j, int l) {
-    return i/3 - (ABS(i-k))*Sign(l-j);
+    return i / 3 - (ABS(i - k)) * Sign(l - j);
 }
 
 int IncreaseJ(int k, int i, int j, int l) {
-    return j%10 - (MAX(i,l))%(k+1);
+    return j % 10 - (MAX(i, l)) % (k + 1);
 }
 
 int IncreaseL(int k, int i, int j, int l) {
-    return i + (j*k)%5 + l/5 + 3;
+    return i + (j * k) % 5 + l / 5 + 3;
 }
 
 int Lab9(int i, int j, int l) {
     int k = 0;
+    int newI;
+    int newJ;
+    int newL;
 
-    while (k<=STEPS) {
+    while (k <= STEPS) {
 
-        if (CheckInArea(i,j)) {
+        if (CheckInArea(i, j)) {
             printf("%d: i = %d; j = %d; l = %d\n", k, i, j, l);
             printf("POPADANIE\n");
             return 1; 
         }
         k++;
-        if (k == STEPS+1) {
+        if (k == STEPS + 1) {
             printf("%d: i = %d; j = %d; l = %d\n", k-1, i, j, l);
             printf("PROMAH\n");
             return 0;
         }
-        int newI = IncreaseI(k-1,i,j,l);
-        int newJ = IncreaseJ(k-1,i,j,l);
-        int newL = IncreaseL(k-1,i,j,l);
+        newI = IncreaseI(k - 1, i, j, l);
+        newJ = IncreaseJ(k - 1, i, j, l);
+        newL = IncreaseL(k - 1, i, j, l);
         i = newI;
         j = newJ;
         l = newL;
@@ -78,7 +81,7 @@ int Lab9(int i, int j, int l) {
 }
 
 int main() {
-    Lab9(I_START_POINT,J_START_POINT,L_START_POINT);
+    Lab9(I_START_POINT, J_START_POINT, L_START_POINT);
 
     return 0;
 }
