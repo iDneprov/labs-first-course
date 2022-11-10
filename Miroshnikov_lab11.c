1b5b07c72d2f158c4980abc5cf7a8563c7cd639f
dmitrijmrsh <115350804+dmitrijmrsh@users.noreply.github.com>
2022-11-11 01:22:36 +0300

    Update Miroshnikov_lab11.c

--- Miroshnikov_lab11.c
+++ Miroshnikov_lab11.c
@@ -5,18 +5,27 @@ int main() {
     int k = 0;
     int ans = 0;
     char a = 0;
+    char fl = a;
     while (1) {
         a = getchar();
         if (a == EOF) {
             if ((k <= SIZE) && (k != 0)) {
                 ans += 1;
                 printf("\n");
+                break;
+            } else {
+                if (fl == '\n') {
+                    break;
+                } else {
+                    printf("\n");
+                    break;
+                }
             }
-            break;
         } else {
             if ((a != ' ') && (a != ',') && (a != '\v') && (a != '\n') && (a != '\t')) {
                 k += 1;
             } else {
+                fl = a;
                 if ((k <= SIZE) && (k != 0)) {
                     ans += 1;
                 }
