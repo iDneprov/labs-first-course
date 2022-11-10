f94e0a29e06c38531d1123f262efbfb538b58b59
dmitrijmrsh <115350804+dmitrijmrsh@users.noreply.github.com>
2022-11-10 13:51:39 +0300

    Update Miroshnikov_lab12.c

--- Miroshnikov_lab12.c
+++ Miroshnikov_lab12.c
@@ -1,32 +1,41 @@
-#include<stdio.h>
+//Принимаю пакет тестов с помощью команды ./a.out < test.txt(в этом файле лежит пакет тестов)
+#include <stdio.h>
 
-int Abs(int a) {
-    return (a >= 0) ? a : -a;
-}
-
-int Check(int *mas, int i) {
-    for (i = 0; i < 10; ++i) {
+int Check(int *mas) {
+    for (int i = 0; i < 10; ++i) {
         if (mas[i] == 0) {
             return -1;
         }
-        return 0;
     }
+    return 0;
 }
 
 int main() {
     int mas[10];
-    int a, ch, i;
-    scanf("%d", &a);
-    a = Abs(a);
-    while (a != 0) {
-        ch = a % 10;
-        mas[ch] += 1;
-        a = a / 10;
-    }
-    if (Check(mas, i) == -1) {
-        printf("%s %s", "YES", "\n");
-    } else {
-        printf("%s %s", "NO", "\n");
+    char a = 0;
+    while (1) {
+        a = getchar();
+        if (a == EOF) {
+            break;
+        } else {
+            if (a != '\n') {
+                mas[a - '0'] += 1;
+            }
+            if (a == '\n') {
+                if (Check(mas) == -1) {
+                    printf("%s %s", "YES", "\n");
+                } else {
+                    printf("%s %s", "NO", "\n");
+                }
+                for (int j = 0; j < 10; ++j) {
+                    mas[j] = 0;
+                }
+            }
+            if (a == '-') {
+                continue;
+            }
+        }
     }
     return 0;
+    printf("%s", "\n");
 }
