eb60e79ac3ba435e5cb08b1ea10e47ae5b4506c1
ZBelka100 <andreyzabelkin333@gmail.com>
2022-11-10 08:34:11 +0300

    Zabelkin-lab9

--- code.c
+++ code.c
@@ -1,45 +1,48 @@
 #include <stdio.h>
 
-#define i_0 -11
-#define j_0 -6
-#define l_0 -5
+#define START_I -11
+#define START_J -6
+#define START_L -5
 
 int FindNewI(int i,int j, int l,int k) {
-    new_i = (i + j + l) * (k + 1) % 25 - (i * j * l * (k + 2) % 10) + 10;
-    return new_i;
+    newI = (i + j + l) * (k + 1) % 25 - (i * j * l * (k + 2) % 10) + 10;
+    return newI;
 }
 
 int FindNewJ(int i,int j, int l,int k) {
-    new_j = (((i + j + l) * (k + 3) % 25) * (((i + j + l) * (k + 3) % 25) >= (i * j * l * (k + 4) % 25))) +
+    newJ = (((i + j + l) * (k + 3) % 25) * (((i + j + l) * (k + 3) % 25) >= (i * j * l * (k + 4) % 25))) +
         (i * j * l * (k + 4) % 25) * ((i * j * l * (k + 4) % 25) > ((i + j + l) * (k + 3) % 25)) + 10;
-    return new_j;
+    return newJ;
 }
 
 int FindNewL(int i,int j, int l,int k) {
-    new_l = 2*((l > 0) - (l < 0)) * ((((i + j + l) * (k + 5) % 10) > (i * j * l * (k + 6) % 25))-
+    newL = 2*((l > 0) - (l < 0)) * ((((i + j + l) * (k + 5) % 10) > (i * j * l * (k + 6) % 25))-
         (((i + j + l) * (k + 5) % 10) < (i * j * l * (k + 6) % 25)))*((i + j + l) * (k + 5) % 10 - i * j * l * (k + 6) % 25);
-    return new_l;
+    return newL;
 }
 
 
 int main(void) {
     int i,j,l,k = 1,flag = 0;
-    i = i_0;
-    j = j_0;
-    l = l_0;
+    i = START_I;
+    j = START_J;
+    l = START_L;
     for (; k < 50; ++k) {
-        new_i = FindNewI(i,j,l,k);
-        new_j = FindNewJ(i,j,l,k);
-        new_l = FindNewL(i,j,l,k);
+        newI = FindNewI(i,j,l,k);
+        newJ = FindNewJ(i,j,l,k);
+        newL = FindNewL(i,j,l,k);
         if ((new_i >= 5) && (new_i <= 15) && (new_j <= -5) && (new_j >= -15)) {
             flag = 1;
             printf("Number of iterations%d\n",k);
             printf("Coordinates of the end of the movement %d %d , motion parameter %d\n",i,j,l );
             break;
+
+
+
         }
-        i = new_i;
-        j = new_j;
-        l = new_l;
+        i = newI;
+        j = newJ;
+        l = newL;
     }
     if (flag == 0) {
         printf("Lost in Saratov\n");
