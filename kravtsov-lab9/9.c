342adc822736efd7686ef2adc46242334d252330
unknown <alekseygkravtsov@yandex.ru>
2022-11-10 14:27:46 +0300

    v3

--- kravtsov-lab9/9.c
+++ kravtsov-lab9/9.c
@@ -14,7 +14,6 @@
 #define MAX(a, b) (a > b) ? a : b 
 #define ABS(a) (a < 0) ? -(a) : a 
 
-
 int Sign(int num) {
     if (num > 0) {
         return 1; 
@@ -27,7 +26,6 @@ int Sign(int num) {
     return -1;  
 }
 
-
 int CheckInArea(int i, int j) {
     
     int firstDistance = (i - I_CENTER_FIRST) * (i - I_CENTER_FIRST) 
@@ -38,7 +36,6 @@ int CheckInArea(int i, int j) {
     
     return (firstDistance <= RADIUS * RADIUS) && (secondDistance <= RADIUS * RADIUS);
 }
-
 
 int IncreaseI(int k, int i, int j, int l) {
     return i / 3 - (ABS(i - k)) * Sign(l - j);
