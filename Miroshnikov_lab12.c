fbf61291ddd3d295040659e1459f40d0dd929611
dmitrijmrsh <115350804+dmitrijmrsh@users.noreply.github.com>
2022-11-11 12:46:47 +0300

    Update Miroshnikov_lab12.c

--- Miroshnikov_lab12.c
+++ Miroshnikov_lab12.c
@@ -4,7 +4,7 @@
 int Check(int *mas) {
     for (int i = 0; i < 10; ++i) {
         if (mas[i] == 0) {
-            return -1;
+            return 1;
         }
     }
     return 0;
@@ -13,19 +13,31 @@ int Check(int *mas) {
 int main() {
     int mas[10];
     char a = 0;
+    char fl = a;
     while (1) {
         a = getchar();
         if (a == EOF) {
-            break;
+            if (fl != '\n') {
+                printf("\n");
+                if (Check(mas)) {
+                    printf("YES\n");
+                } else {
+                    printf("NO\n");
+                }
+                break;
+            } else {
+                break;
+            }
         } else {
+            fl = a;
             if (a != '\n') {
                 mas[a - '0'] += 1;
             }
             if (a == '\n') {
-                if (Check(mas) == -1) {
-                    printf("%s %s", "YES", "\n");
+                if (Check(mas)) {
+                    printf("YES\n");
                 } else {
-                    printf("%s %s", "NO", "\n");
+                    printf("NO\n");
                 }
                 for (int j = 0; j < 10; ++j) {
                     mas[j] = 0;
@@ -37,5 +49,4 @@ int main() {
         }
     }
     return 0;
-    printf("%s", "\n");
 }
