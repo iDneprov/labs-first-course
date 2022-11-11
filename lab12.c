6b91df8f0952cca309fda60d4377482adf2bafb8
Irina <sekti2004@yandex.ru>
2022-11-12 01:32:55 +0300

    final commit

---
+++ lab12.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+#define ZERO '0'
+#define DEFOLD 0
+#define NUMBEROFTESTS 10
+
+int PowInt(int number, int degree) {
+    int result = 1;
+    while (degree > 0) {
+        result *= number;
+        --degree;
+    }
+    return result;
+}
+
+int SeparatorSymbols(char symbol) {
+    return symbol == '\n' || symbol == '\t' || symbol == ' ';
+}
+
+int main(void) {
+    int number = DEFOLD;
+    int write = 1;
+    int countZero = DEFOLD;
+    int symbol = getchar();
+    while (symbol != EOF) {
+        if (write) {
+            if ((char) symbol == ZERO && number == DEFOLD) {
+                ++countZero;
+                symbol = getchar();
+            } else if (SeparatorSymbols(symbol)) {
+                symbol = getchar();
+                while (SeparatorSymbols(symbol)) {
+                    symbol = getchar();
+                }
+                write = 0;
+            } else if (symbol == EOF) {
+                write = 0;
+            } else {
+                number = number * 10 + (symbol - ZERO);
+                symbol = getchar();
+            }
+        } 
+        if (!write) {
+            int numberInTwoSystem = DEFOLD, countRang = DEFOLD;
+            while (number > DEFOLD) {
+                numberInTwoSystem += ((number % 2) * PowInt(10, countRang));
+                ++countRang;
+                number /= 2;
+            }
+            int nowRangNumber, middle = countRang / 2, isZero = 1;
+            while (numberInTwoSystem > DEFOLD || countRang > DEFOLD) {
+                isZero = 0;
+                nowRangNumber = PowInt(10, countRang - 1);
+                char res = ZERO + numberInTwoSystem / nowRangNumber;
+                putchar(res);
+                numberInTwoSystem %= nowRangNumber;
+                --countRang;
+                if (countRang == middle) {
+                    while (countZero > DEFOLD) {
+                        putchar(ZERO);
+                        --countZero;
+                    }
+                }
+            }
+            if (isZero) {
+                while (countZero > DEFOLD) {
+                    putchar(ZERO);
+                    --countZero;
+                }
+            }
+            putchar('\n');
+            write = 1;
+        }
+    }
+}
