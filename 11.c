4c94aa044276a7f3af7deff65999e38f760949f2
Bonik0 <115299174+Bonik0@users.noreply.github.com>
2022-11-12 01:55:21 +0300

    Add files via upload

--- 11.c
+++ 11.c
@@ -1,95 +1,99 @@
 #include <stdio.h> //lab 11 variant 
 
 char CheckTub(char s) {
-  return ((int)s < 33 || s == ' ' || s == '.' || s == ',') ? 1 : 0;
+    return (s < 33 || s == ' ' || s == '.' || s == ',');
 }
 
 char FindTubs(char sLast) {
-  char s;
-  while (s != EOF) {
-    s = getchar();
-     if (((int)s > 47 && (int)s < 58) && CheckTub(sLast)) {
-      return s;
+    char s = getchar();
+    while (s != EOF) {
+        if (((s > 47 && s < 58) || s == '-') && CheckTub(sLast)) {
+            return s;
+        }
+        sLast = s;
+        s = getchar();
     }
-    sLast = s;
-  }
-  return s;
+    return s;
 }
 
 int FindNum() {
-  char s = FindTubs(' ');
-  int firstNum = -2;
-  int sim;
-  while (CheckTub(s) == 0) {
-    if (firstNum != -2) {
-      s = FindTubs(s);
+    char s = FindTubs(' ');
+    int firstNum = -2, flagMinus = 0;
+    while (!CheckTub(s)) {
+        flagMinus = 0;
+        if (firstNum != -2) {
+            s = FindTubs(s);
+        }
+        if (s == '-') {
+            flagMinus = 1;
+            s = FindTubs(' ');
+        }
+        firstNum = -1;
+        while (s > 47 && s < 58) {
+            if (firstNum == -1) {
+                firstNum++;
+            }
+            firstNum = firstNum * 10 + s - 48;
+            s = getchar(); 
+        }
     }
-  firstNum = -1;
-  sim = (int)s;
-  while (sim > 47 && sim < 58) {
-    if (firstNum == -1) {
-      firstNum++;
-    }
-    firstNum = firstNum * 10 + sim - 48;
-    s = getchar();
-    sim = (int)s;
-  }
-  }
-  return (firstNum < 0) ? -1 : firstNum;
+    return (firstNum < 0) ? -1 : (flagMinus) ? -firstNum * 2 : firstNum * 2;
 }
 
 int Sum(int num) {
-  int sum = 0, tens = 10;
-
-  while (num / tens != 0) {
-    sum += num % tens;
-    num /= tens;
-  }
-  sum += num % tens;
-  return sum;
+    int sum = 0;
+    if (num < 0) {
+        num *= -1;
+    }
+    while (num > 9) {
+        sum += num - (num / 10) * 10;
+        num /= 10;
+    }
+    return sum + num;
 }
 
 int FindSecondToLast(int first, int second) {
-  int flag = 1;
-  int firstCheck, secondCheck;
-  while (1) {
-    if(flag == 1){
-      firstCheck = FindNum();
-      if (firstCheck < 0){
-        break;
-      }
-      first = firstCheck;
-      flag = 0;
-    } else {
-      secondCheck = FindNum();
-      if(secondCheck < 0){
-        break;
-      }
-        second = secondCheck;
-        flag = 1;
-      }
-  }
-  return (flag) ? first : second;
+    int flag = 1;
+    int firstCheck, secondCheck;
+    while (1) {
+        if (flag) {
+            firstCheck = FindNum();
+            if (firstCheck == -1) {
+                break;
+            }
+            first = firstCheck;
+            flag = 0;
+        } else {
+            secondCheck = FindNum();
+            if (secondCheck == -1) {
+                break;
+            }
+            second = secondCheck;
+            flag = 1;
+        }
+    }
+    return (flag) ? first : second;
 }
 
 int SecondToLastNumber() {
-  int firstNum, secondNum;
-  firstNum = FindNum();
-    if(firstNum == -1){
-    printf("\nno decimal numbers\n");
-    return -1;
+    int firstNum, secondNum;
+    firstNum = FindNum();
+    if (firstNum == -1) {
+        printf("\nno decimal numbers\n");
+        return -1;
     }
-  secondNum = FindNum();
- if(secondNum == -1){
-    printf("\none decimal numbers:%d\n", firstNum);
-    return -1;
- }
-  return FindSecondToLast(firstNum, secondNum); 
+    secondNum = FindNum();
+    if (secondNum == -1) {
+        firstNum /= 2;
+        printf("\none decimal numbers:%d\n", firstNum);
+        return -1;
+    }
+    return FindSecondToLast(firstNum, secondNum); 
 }
 
 int main(void) {
-  int anser = SecondToLastNumber();
-  if(anser != -1){
-  printf("\nNumber: %d Sum: %d\n", anser, Sum(anser));
-  }
+    int anser = SecondToLastNumber() / 2;
+    if (anser != 0) {
+        printf("\nNumber: %d Sum: %d\n", anser, Sum(anser));
+    }
 }
