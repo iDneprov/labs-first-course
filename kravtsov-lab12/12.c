284fa2013b7875b25b5ea391e9d8568df36cc0f3
unknown <alekseygkravtsov@yandex.ru>
2022-11-12 16:33:18 +0300

    v2

--- kravtsov-lab12/12.c
+++ kravtsov-lab12/12.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 unsigned long Lab12(unsigned long n) {
+    if (n == 0) {
+        return 101;
+    };
     unsigned long a = (n * 10 + 1);
     unsigned long x = 10;
     for( ; x <= a ; ) {
         x = x * 10;
     };
-    
+
     return x + a;
 }
 
