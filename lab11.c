782d68b57e17606bc5c9fe40cfdef6160170042f
Lenina-Paket <115350805+Lenina-Paket@users.noreply.github.com>
2022-11-12 16:28:16 +0300

    Update lab11.c

--- lab11.c
+++ lab11.c
@@ -2,21 +2,33 @@
 
 int main(void) {
     char c; 
-    int len = 0, sum = 0;
+    int len = 0, sum = 0, flag = 1;
     c = getchar();
-    while (c != EOF) {
-        if ((c != ' ') && (c != ',') && (c != '.') && (c != '\n')) {
+    while (1) {
+        if ((c != ' ') && (c != ',') && (c != '.') && (c != '\n') && (c != EOF)) {
             len++;
         } else {
-            if (len >= 3) {
+            if (c != EOF){
+                if (len >= 3) {
                 sum++;  
             }
             len = 0;
+            } else {
+                if (len >= 3) {
+                sum++;  
+            }
+                printf("%d\n", sum);
+                flag = 0;
+                break;
+            }
             }  
         c = getchar();
         }
-        printf("%d\n", sum);
-    if (len >= 3) {
+        if (flag){
+            flag = 0;
+            printf("%d\n", sum);
+        }
+    if (len >= 3 && flag) {
         printf("%d\n", 1);
     }
 return 0;   
