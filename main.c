a9ec07d4c802b6c9ea2314b6213ae36f5897dca0
ananasik558 <denchickshow@list.ru>
2022-11-12 15:34:19 +0300

    new

--- main.c
+++ main.c
@@ -8,31 +8,37 @@ int main() {
         if(elem == EOF) {
             break;
         }
-        if(elem != ' ') {
-            if(elem != '-' && elem != '0') {
-                int l = 0, number = 0;
-                while(l < 6) {
-                    if(elem == ' ') {
-                        break;
-                    }
-                    number = number * 10 + elem - 48;
-                    l += 1;
-                    elem = getchar();
-                    if(elem == EOF) {
-                        break;
-                    }
+        
+        int koef = 1;
+
+        if(elem == '-' || elem == '0') {
+            koef = 0;
+        }
+        if(elem >= '0' && elem <= '9') {
+
+            int l = 0, number = 0;
+
+            while(l < 6) {
+                if(elem == ' ') {
+                    break;
                 }
-                if(number <= 65535) {
-                        k += 1;
-                }
-            }
-            while(elem != ' ') {
+                number = number * 10 + elem - 48;
+                l += 1;
                 elem = getchar();
                 if(elem == EOF) {
-                        break;
-                    }
+                    break;
+                }
+            }
+            if(number <= 65535) {
+                k += koef;
             }
         }
+        while(elem != ' ') {
+            elem = getchar();
+            if(elem == EOF) {
+                break;
+            }
+        }      
     }
     printf("%d\n", k);
     return 0;
