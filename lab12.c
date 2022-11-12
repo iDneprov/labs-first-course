ac58329c61c0536fafd0e5aba1bb38aa5bfa9135
Lenina-Paket <115350805+Lenina-Paket@users.noreply.github.com>
2022-11-12 13:55:04 +0300

    Update lab12.c

--- lab12.c
+++ lab12.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+int IsEvenDigit(char c) {
+    return (c >= '0') && (c <= '9') && ((c - '0') % 2 != 0);
+}
+
 int main(void) {
     char c;
     c = getchar();
     while (c != EOF) {
-        if (c == '-' || c == '1' || c == '3' || c == '5' || c == '5' || 
-        c == '7' || c == '9'){
+        if (IsEvenDigit(c)) {
             putchar(c);
         }
         if (c == '\n'){
