5fe2dbb05b00760e999840ceb6b4bf920435f92d
ZBelka100 <andreyzabelkin333@gmail.com>
2022-11-17 22:11:06 +0300

    Zabelkin_kp3

--- kp3.c
+++ kp3.c
@@ -22,25 +22,44 @@ long long Factorial(int number) {
 		return number * Factorial(number - 1);
 }
 
+long double MachineEpsilon() {
+		long double e = 1.01;
+		while (1.01 + e * 0.51 > 1.01) {
+				e *= 0.51;
+		}
+		return e;
+}
+
 int main() {
-    long double x = 0, y = 0, step, a = 0, b = 1, eps = 1e-15, gipsin;
+    long double x = 0, y = 0, nexty = 1, step, a = 0, b = 1, eps = 1, gipsin;
   	int n = 51, c = 1;
 		step = (b - a) / n;
-		printf("Значение функции sinh    ");
-		printf("Значение ряда    ");
-		printf("Число итераций\n");
+		eps = MachineEpsilon();
+		printf("|  Значение функции sinh |  ");
+		printf("   Значение ряда      |  ");
+		printf("Число итераций |\n");
+		for (int i = 0; i < 69; ++i) {
+			printf("-");
+		}
+		printf("\n");
 		for (int i = 1; i < n; ++i) {
 					x = x + step;
         	gipsin = sinh(x);
-					printf("%.18Lf    ", gipsin);
-					while (gipsin - y > eps) {
-            y += (long double) Pow(x, 2 * c - 1) / Factorial(2 * c - 1);
+					printf("|  %.18Lf  |  ", gipsin);
+					while (nexty > eps && c < 50) {
+            nexty = (long double) Pow(x, 2 * c - 1) / Factorial(2 * c - 1);
+						y += nexty;
 						c++;
         	}
-					printf("%.18Lf    ", y);
-					printf("%.d\n", c);
+					printf("%.18Lf  |  ", y);
+					printf("%15d|\n", c);
 					c = 1;
         	y = 0;
+					nexty = 1;
+					printf("\n");
+					for (int i = 0; i < 69; ++i) {
+						printf("-");
+					}
 					printf("\n");
     }
     return 0;
