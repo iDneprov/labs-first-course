2ca316b8fd5eedcdc54a97ec7c7538b946bf5387
ZBelka100 <115878049+ZBelka100@users.noreply.github.com>
2022-11-21 21:56:45 +0300

    Update kp3.c

--- kp3.c
+++ kp3.c
@@ -10,31 +10,34 @@ long double MachineEpsilon() {
 }
 
 int main() {
-	for (int i = 0; i < 69; ++i) {
-		printf("-");
-	}
-	printf("\n");
-	for (int i = 1; i < n; ++i) {
-		x = x + step;
-        gipsin = sinh(x);
-		y = x;
-		nexty = x;
-		printf("|  %.18Lf  |  ", gipsin);
-		while (nexty > eps && c < 101) {
-            nexty = nexty*x*x/((2 * c) * (2*c + 1));
-			y += nexty;
-			c++;
+    long double x = 0, y, nexty, step, a = 0, b = 1, eps, gipsin;
+  	int n = 100, c = 1;
+		step = (b - a) / n;
+		eps = MachineEpsilon();
+		printf("|  Значение функции sinh |  ");
+		printf("   Значение ряда      |  ");
+		printf("Число итераций |\n");
+		for (int i = 1; i < n; ++i) {
+			x = x + step;
+        	gipsin = sinh(x);
+			y = x;
+			nexty = x;
+			printf("|  %.18Lf  |  ", gipsin);
+			while (nexty > eps && c < 101) {
+        	    nexty = nexty*x*x/((2 * c) * (2*c + 1));
+				y += nexty;
+				c++;
         	}
-		printf("%.18Lf  |  ", y);
-		printf("%15d|\n", c);
-		c = 1;
-        y = 0;
-		nexty = 1;
-		printf("\n");
-		for (int i = 0; i < 69; ++i) {
-			printf("-");
-		}
-		printf("\n");
+			printf("%.18Lf  |  ", y);
+			printf("%15d|\n", c);
+			c = 1;
+        	y = 0;
+			nexty = 1;
+			printf("\n");
+			for (int i = 0; i < 69; ++i) {
+				printf("-");
+			}
+			printf("\n");
     }
     return 0;
 }
