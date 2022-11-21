7bba25e3eb46ff2ac9f429f19b2470af2aba9792
Tigran Badalyan <t2@MacBook-Air-Tigran.local>
2022-11-21 21:20:52 +0300

    the code was modified

--- Lab11.c
+++ Lab11.c
@@ -11,106 +11,105 @@ void Lab11(void) {
     int lastNum = 0;
     int tmpLastNum = 0;
 
-    int statusFirst = 0;
+    int status = 0;
     int statusMinus = 0;
     
     int i = 0;
     while (symb != EOF) {        
-        //printf("\nsymb == %c\n", symb);
+        symbLast = symb;
+        symb = getchar();
         
-        if ((i == 1 && isdigit(symb)) || symb == '-' || symb == ' ' || (symbLast == ' ' && isdigit(symb))) {        
-            //printf("\nsymb_if == %c\n", symb);
-            if (isdigit(symb) || symb == '-') {
-                if (statusFirst == 0) {
-                    if (symb == '-')
-                        ++statusMinus;
-                    
-                    if (isdigit(symb))
+        if (status == 0 || status == 2) {
+            if ((i == 0 && isdigit(symb)) || symb == '-' || symb == ' ' || (symbLast == ' ' && isdigit(symb))) {        
+                if (status == 0)
+                    status = 1;
+                if (status == 2)
+                    status = 3;
+                //printf("\nstatus = %d\n", status);
+                
+                if (symb == '-')
+                    statusMinus = 1;
+
+                if (isdigit(symb)) {
+                    if (status == 1)
                         firstNum = symb - '0';
                     
-                    symb = getchar();
+                    tmpLastNum = symb - '0';
+                    //printf("tmpNumFirst = %d\n", tmpLastNum);
+                }
 
-                    while (true) {
-                        //printf("\nsymb_in_first == %c\n", symb);
-                        
-                        if (isdigit(symb)) {    
-                            firstNum = firstNum * 10 + (symb - '0');
-                        } else {                         
-                            //printf("\nsymb_first == %c\n", symb);
-                            
-                            if (symb == EOF) {
-                                ++statusFirst;
-                                break;
-                            }
-                            
-                            if (symb != ' ') {
-                                firstNum = 0;
-                                break;
-                            }
-                            
-                            ++statusFirst;
-                            break;
-                        }
+                symb = getchar();
+            }
+        }
+        
+        if (status == 1 || status == 3) {
+            if (isdigit(symb)) {
+                if (status == 1)
+                    firstNum = firstNum * 10 + (symb - '0');
 
-                        symb = getchar();        
-                    }
-                    //printf("\nsymb_out_first == %c\n", symb);
+                printf("tmpNumNow = %d\n", tmpLastNum);    
+                tmpLastNum = tmpLastNum * 10 + (symb - '0');
+                printf("tmpNumNow_late = %d\n", tmpLastNum);
+            } else {
+                //printf("in\n");
+                //printf("tmplastNumEnd = %d\n", tmpLastNum);
+                if (symb == EOF) {
+                    lastNum = tmpLastNum;
                     
                     if (statusMinus == 1) {
-                        firstNum = -1 * firstNum; 
+                        if (status == 1) 
+                            firstNum = -1 * firstNum; 
+                    
+                        lastNum = -1 * lastNum;
+
                         statusMinus = 0;
                     }
-                } else {
-                    if (symb == '-')
-                        ++statusMinus;
                     
-                    if (isdigit(symb))
-                        tmpLastNum = symb - '0';
-                    
-                    symb = getchar();
-                    
-                    while (true) {
-                        //printf("\nsymb_in_last== %c\n", symb);
-                        if (isdigit(symb)) { 
-                            tmpLastNum = tmpLastNum * 10 + (symb - '0');
-                        } else {
-                            //printf("\nsymb_last == %c\n", symb);
-
-                            if (symb == EOF) {
-                                lastNum = tmpLastNum;
-                                break;
-                            }
-
-                            if (symb != ' ') {
-                                if (lastNum == 0)    
-                                    lastNum = firstNum;
-                                break;
-                            }
-                            
-                            lastNum = tmpLastNum;
-                            tmpLastNum = 0;
-                                
-                            break;
-                        }
-
-                        symb = getchar();
-                    }                
-                    //printf("\nsymb_out_last== %c\n", symb);
-                    
-                    if (statusMinus == 1) {
-                        lastNum = -1 * lastNum; 
-                        statusMinus = 0;            
-                    }
+                    break;
                 }
+
+                if (symb != ' ') {
+                    if (status == 1) {
+                        firstNum = 0;
+                        status = 0;
+                    }
+                    if (status == 3)
+                        status = 2;
+
+                    statusMinus = 0;
+                }
+            
+                //printf("\n firstNum_End = %d\n", firstNum);
+                //printf("lastNum_End = %d\n", lastNum);
+
+                if (status == 1 || status == 3) {
+                    //printf("tmplastNumEnd_leter = %d\n", tmpLastNum);
+                    lastNum = tmpLastNum;
+                    //printf("lastNumEnd = %d\n", lastNum);
+                    
+                    status = 2;
+                }
+                
+                tmpLastNum = 0;
+                //printf("tmplastNumEnd_0 = %d\n", tmpLastNum);
+                //printf("lastNumEnd_0 = %d\n", lastNum);
+
+
+                if (statusMinus == 1) {
+                    if (status == 1) 
+                        firstNum = -1 * firstNum; 
+                    
+                    lastNum = -1 * lastNum;
+
+                    statusMinus = 0;
+                }
+                //printf("\n firstNum = %d\n", firstNum);
+                //printf("lasttNum = %d\n", lastNum);
             }   
         }
         
-        symbLast = symb;
-        
-        symb = getchar();
-        
         ++i;
-    }
+    } 
     
     printf("\n%d\n", firstNum);
     printf("%d\n", lastNum);
