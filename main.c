166f8e9026e4e0932c5d6ab1732a4baaec60e461
ananasik558 <denchickshow@list.ru>
2022-11-23 14:11:18 +0300

    version 0

--- main.c
+++ main.c
@@ -1,63 +1,34 @@
 #include <stdio.h>
-#include <ctype.h>
 
-#define VOWELS (1u << ('a' - 'a') | 1u << ('o' - 'a') | 1u << ('i' - 'a') | 1u << ('e' - 'a') | 1u << ('u' - 'a') | 1u << ('y' - 'a'))
-
-unsigned int CharToSet(char s) {
-    s = tolower(s);
-    if (s < 'a' || s > 'z') {
-        return 0;
-    } else {
-        return 1u << (s - 'a');
-    }
-}
-
-unsigned int CheckWord(unsigned int word) {
-    char letter;
-    int k = 0;
-    for (letter = 'a'; letter <= 'z'; letter++) {
-        if ((word & CharToSet(letter)) != 0) {
-            k = k + 1;
-        }
-    }
-    if (k == 1) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
+enum {MaxSize = 100};
 
 int main() {
-    int s = 0, result = 0, flag = 1;
-    unsigned int lettersFromWord = 0;
-    while (1) {
-        s = getchar();
-        if (s == ' ' || s == '\n' || s == EOF) {
-            lettersFromWord = lettersFromWord & ~VOWELS;
-            result = result + CheckWord(lettersFromWord);
-            if (result != 0 && flag != 0) {
-                break;
-            }
-             else {
-                result = 0;
-             }
-            lettersFromWord = 0;
-            flag = 1;
-        } else {
-            if ((CharToSet(s) & ~VOWELS & lettersFromWord) == 0) {
-            lettersFromWord = (lettersFromWord | CharToSet(s)) & ~VOWELS;
-            } else {
-                flag = 0;
-            }
-        }
-        if (s == EOF) {
-            break;
+    int mas[MaxSize][MaxSize];
+    int n = 0, f = getchar(), j = 0;
+    int dir[4] = {-1, 0, 1, 0};
+    while (f != '\n') {
+        n += 1;
+        scanf("%d", &mas[0][j]);
+        j += 1;
+        f = getchar();
+    }
+    printf("%d\n", n);
+    for(int i = 1; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            scanf("%d", &mas[i][j]);
         }
     }
-    if (result == 1) {
-        printf("Yes. There are word with one consonant letter");
-    } else {
-        printf("No words with one consonant letter");
+    int l = n;
+    int p = 0;
+    int i = n; j = 0;
+    while(l != 0) {
+        for(int k = 0; k < l; ++k) {
+            i += dir[p % 4];
+            j += dir[(p+1) % 4];
+            printf("%d ", mas[i][j]);
+        }
+        ++p;
+        l -=p % 2;
     }
     putchar('\n');
 }
