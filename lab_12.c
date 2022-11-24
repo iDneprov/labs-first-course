9e2898f86d25eb05d81ef50693e4600b2e15858a
MaratS <cool.marats@yandex.ru>
2022-11-24 20:55:07 +0300

    Sisenov-lab12

---
+++ lab_12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+    char a, b;
+    long long int c = 0;
+
+        while (b != EOF) {
+            b = getchar();
+            
+            if ((b != ' ') && (b != ',') && (b != '\n') && (b != EOF) && (b != '\t') && (b != '\v')) {
+                a = b;
+                c = c * 10 + (b - '0');
+            } else {
+                c = c / 10;
+                if (c != 0) {
+                    printf("%c%d", a, c);
+                    printf("%c", b);
+                    c = 0;
+                } else {
+                    printf("%c%c", a, b);
+                }
+            }
+        };
+
+    return 0;
+
+}
