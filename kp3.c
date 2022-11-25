4fc5100e17f333085f2022984fd15dbb1423509f
ZBelka100 <115878049+ZBelka100@users.noreply.github.com>
2022-11-25 10:18:07 +0300

    Update kp3.c

--- kp3.c
+++ kp3.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+#define STEP 0.005l
+
 long double MachineEpsilon() {
 	long double e = 1.01;
 	while (1.01 + e * 0.51 > 1.01) {
@@ -10,34 +12,34 @@ long double MachineEpsilon() {
 }
 
 int main() {
-    long double x = 0, y, nexty, step, a = 0, b = 1, eps, gipsin;
-  	int n = 100, c = 1;
-		step = (b - a) / n;
-		eps = MachineEpsilon();
-		printf("|  Значение функции sinh |  ");
-		printf("   Значение ряда      |  ");
-		printf("Число итераций |\n");
-		for (int i = 1; i < n; ++i) {
-			x = x + step;
-        	gipsin = sinh(x);
-			y = x;
-			nexty = x;
-			printf("|  %.18Lf  |  ", gipsin);
-			while (nexty > eps && c < 101) {
-        	    nexty = nexty*x*x/((2 * c) * (2*c + 1));
-				y += nexty;
-				c++;
-        	}
-			printf("%.18Lf  |  ", y);
-			printf("%15d|\n", c);
-			c = 1;
-        	y = 0;
-			nexty = 1;
-			printf("\n");
-			for (int i = 0; i < 69; ++i) {
-				printf("-");
-			}
-			printf("\n");
+  	long double x = 0, y, nexty, step, a = 0, b = 1, eps, gipsin;
+	int c = 1;
+	eps = MachineEpsilon();
+	printf("|  Значение функции sinh |  ");
+	printf("   Значение ряда      |  ");
+	printf("Число итераций |");
+    printf(" Значение x |\n");
+	while (x < b-STEP) {
+		x = x + STEP;
+       	gipsin = sinh(x);
+		y = x;
+		nexty = x;
+		printf("|  %.18Lf  |  ", gipsin);
+		while (nexty > eps && c < 101) {
+       	    nexty = nexty*x*x/((2 * c) * (2*c + 1));
+			y += nexty;
+			c++;
+       	}
+		printf("%.18Lf  |  ", y);
+		printf("%15d|", c);
+        printf("%12Lf|\n", x);
+		c = 1;
+       	y = 0;
+		nexty = 1;
+		for (int i = 0; i < 82; ++i) {
+			printf("-");
+		}
+		printf("\n");
     }
     return 0;
 }
