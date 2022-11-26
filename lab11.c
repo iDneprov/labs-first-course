960dc5b603f4b95352d38f9ab44d53517b84d3db
F0R3D0 <stephen.foredo@gmail.com>
2022-11-26 13:54:44 +0300

    lab11

--- lab11.c
+++ lab11.c
@@ -1,132 +1,138 @@
 #include <stdio.h>
-typedef enum {
-    S0, S1, S2, S3, S4, S5, S6
-} State;
-int isSign(char c)
-{
+ 
+int IsSign(char c) {
     if (c == '+' || c == '-') {
         return 1;
     }
     return 0;
 }
-int isNum(char c)
-{
+ 
+int IsNum(char c) {
     if (c <= '9' && c >= '1') {
         return 1;
     }
     return 0;
 }
-int isSpace(char c)
-{
+ 
+int IsSpace(char c) {
     if (c == ' ' || c == '\n' || c == '\t') {
         return 1;
     }
     return 0;
 }
-int main(void)
-{
+ 
+/* PREVSYMB
+ * ' ' init of program
+ * '1' number
+ * 'a' alpha
+ * '_' space
+ * '-' sign
+ * 'n' num or zero
+ * '0' zero
+ */
+int main(void) {
     char c;
     long long a = 0;
-    State state = S0;
+    char prevSymb = ' ';
     while ((c = getchar()) != EOF) {
-        switch (state) {
-            case S0:
+        switch (prevSymb) {
+            case ' ':
                 printf("\n");
-                if (isSign(c)) {
-                    printf("%c", c);
-                    state = S4;
-                } else if (isNum(c)) {
+                if (IsSign(c)) {
+                    putchar(c);
+                    prevSymb = '-';
+                } else if (IsNum(c)) {
                     a = a * 10 + (c - '0');
-                    state = S1;
+                    prevSymb = '1';
                 } else if (c == '0') {
-                    printf("%c", c);
-                    state = S6;
+                    putchar(c);
+                    prevSymb = '0';
                 } else {
-                    printf("%c", c);
-                    state = S2;
+                    putchar(c);
+                    prevSymb = 'a';
                 }
                 break;
-            case S3:
+            case '_':
                 a = 0;
-                if (isSign(c)) {
-                    printf("%c", c);
-                    state = S4;
-                } else if (isNum(c)) {
+                if (IsSign(c)) {
+                    putchar(c);
+                    prevSymb = '-';
+                } else if (IsNum(c)) {
                     a = a * 10 + (c - '0');
-                    state = S1;
+                    prevSymb = '1';
                 } else if (c == '0') {
-                    printf("%c", c);
-                    state = S6;
-                } else if (isSpace(c)) {
-                    printf("%c", c);
-                    state = S3;
+                    putchar(c);
+                    prevSymb = '0';
+                } else if (IsSpace(c)) {
+                    putchar(c);
+                    prevSymb = '_';
                 } else {
-                    printf("%c", c);
-                    state = S2;
+                    putchar(c);
+                    prevSymb = 'a';
                 }
                 break;
-            case S1:
-                if (isNum(c) || c == '0') {
+            case '1':
+                if (IsNum(c) || c == '0') {
                     a = a * 10 + (c - '0');
-                    state = S1;
-                } else if (isSpace(c)) {
+                    prevSymb = '1';
+                } else if (IsSpace(c)) {
                     printf("0%lld%c", a, c);
-                    state = S3;
+                    prevSymb = '_';
                 } else {
                     printf("%lld%c", a, c);
-                    state = S2;
+                    prevSymb = 'a';
                 }
                 break;
-            case S2:
-                if (isSpace(c)) {
-                    printf("%c", c);
-                    state = S3;
+            case 'a':
+                if (IsSpace(c)) {
+                    putchar(c);
+                    prevSymb = '_';
                 } else {
-                    printf("%c", c);
-                    state = S2;
+                    putchar(c);
+                    prevSymb = 'a';
                 }
                 break;
-            case S5:
-                if (isNum(c) || c == '0') {
+            case 'n':
+                if (IsNum(c) || c == '0') {
                     a = a * 10 + (c - '0');
-                    state = S5;
-                } else if (isSpace(c)) {
+                    prevSymb = 'n';
+                } else if (IsSpace(c)) {
                     printf("0%lld%c", a, c);
-                    state = S3;
+                    prevSymb = '_';
                 } else {
                     printf("%lld%c", a, c);
-                    state = S2;
+                    prevSymb = 'a';
                 }
                 break;
-            case S6:
-                if (isSpace(c)) {
+            case '0':
+                if (IsSpace(c)) {
                     printf("0%c", c);
-                    state = S3;
+                    prevSymb = '_';
                 } else if (c == '0') {
                     printf("0");
-                    state = S6;
-                } else if (isNum(c)) {
+                    prevSymb = '0';
+                } else if (IsNum(c)) {
                     a = a * 10 + (c - '0');
-                    state = S1;
+                    prevSymb = '1';
                 } else {
-                    printf("%c", c);
-                    state = S2;
+                    putchar(c);
+                    prevSymb = 'a';
                 }
                 break;
-            
-            case S4:
-                if (isNum(c)) {
+ 
+            case '-':
+                if (IsNum(c)) {
                     a = a * 10 + (c - '0');
-                    state = S5;
-                } else if (isSpace(c)) {
-                    printf("%c", c);
-                    state = S3;
+                    prevSymb = 'n';
+                } else if (IsSpace(c)) {
+                    putchar(c);
+                    prevSymb = '_';
                 } else if (c == '0') {
-                    printf("%c", c);
-                    state = S6;
+                    putchar(c);
+                    prevSymb = '0';
                 } else {
-                    printf("%c", c);
-                    state = S2;
+                    putchar(c);
+                    prevSymb = 'a';
                 }
         }
     }
