1729150f688e327717530924b6bfff1c971ae399
dmitrijmrsh <115350804+dmitrijmrsh@users.noreply.github.com>
2022-11-27 19:06:51 +0300

    Update Miroshnikov-kp3.c

--- Miroshnikov-kp3.c
+++ Miroshnikov-kp3.c
@@ -23,9 +23,11 @@ long double F(long double x) {
 long double Pow(long double x, long double n, long double i) {
     if (n == 0) {
         return 1;
-    } else if (n == 1) {
+    }
+    else if (n == 1) {
         return x;
-    } else if (n >= 1) {
+    }
+    else if (n >= 1) {
         return x * i;
     }
 }
@@ -41,26 +43,26 @@ int main() {
     step = (b - a) / iter;
     printf("Machine epsilon for long double = %.8Le\n", e);
     printf("Taylor series values table for f(x) = (2x-3)/(x-1)^2\n");
-	printf(" ---------------------------------------------------------------\n");
-	printf("| x \t| sum of line\t\t| function\t\t| iter  |\n");
-	printf(" -------+-----------------------+-----------------------+-------\n");
-	for (long double i = a; i <= b; i += step) {
-	    x = i;
-	    for (int n = 0; n < 999; ++n) {
-	       temp = -(n + 3) * Pow(x, n, i);
-	       result += temp;
-	       count += 1;
-	       if (n > 1) {
-	           x *= i;
-	       }
-	       if (Abs(temp) < e) {
-	           break;
-	       }
-	    }
-	   printf("| %.3Lf\t| %.19Lf| %.19Lf| %d\t|\n", i, result, F(i), count);
-	   result = 0;
-	   count = 0;
-	}
-	printf(" ----------------------------------------------------------------\n");
-	return 0;
+    printf(" ---------------------------------------------------------------\n");
+    printf("| x \t| sum of line\t\t| function\t\t| iter  |\n");
+    printf(" -------+-----------------------+-----------------------+-------\n");
+    for (long double i = a; i <= b; i += step) {
+        x = i;
+        for (int n = 0; n < 999; ++n) {
+            temp = -(n + 3) * Pow(x, n, i);
+            result += temp;
+            count += 1;
+            if (n > 1) {
+                x *= i;
+            }
+            if (Abs(temp) < e) {
+                break;
+            }
+        }
+        printf("| %.3Lf\t| %.19Lf| %.19Lf| %d\t|\n", i, result, F(i), count);
+        result = 0;
+        count = 0;
+    }
+    printf(" ----------------------------------------------------------------\n");
+    return 0;
 }
