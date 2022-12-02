6390a268c4e3af3f77d771613e7a86b61d219089
chavapchacha <lizahlebnickova@yandex.ru>
2022-12-02 10:54:52 +0300

    update lab13.c

--- lab13.c
+++ lab13.c
@@ -14,19 +14,19 @@ unsigned long long MakeSet(wint_t symb) {
 
 int main(void) {
     setlocale(LC_ALL, "");
-    unsigned long long other_vowels = (1llu << (L'а' - L'а') | 1llu << (L'о' - L'а') | 1llu << (L'у' - L'а') | 1llu << (L'э' - L'а') | 1llu << (L'ы' - L'а') | 1llu << (L'и' - L'а'));
-    unsigned long long vowels_type_2 = (1llu << (L'е' - L'а') | 1llu << (L'ё' - L'а') | 1llu << (L'ю' - L'а') | 1llu << (L'я' - L'а'));
-    unsigned long long set_of_letters = 0;
+    unsigned long long otherVowels = (1llu << (L'а' - L'а') | 1llu << (L'о' - L'а') | 1llu << (L'у' - L'а') | 1llu << (L'э' - L'а') | 1llu << (L'ы' - L'а') | 1llu << (L'и' - L'а'));
+    unsigned long long vowelsType2 = (1llu << (L'е' - L'а') | 1llu << (L'ё' - L'а') | 1llu << (L'ю' - L'а') | 1llu << (L'я' - L'а'));
+    unsigned long long setOfLetters = 0;
     wint_t symb = 0;
     char flag = 0;
     while (1) {
         symb = getwchar();
-        set_of_letters = set_of_letters | MakeSet(symb);
-        if ((symb == ' ' || symb == '\n' || symb == ',' || symb == WEOF) && (set_of_letters > 0)) {
-            if ((set_of_letters & other_vowels) == 0) {
+        setOfLetters = setOfLetters | MakeSet(symb);
+        if ((symb == ' ' || symb == '\n' || symb == ',' || symb == WEOF) && (setOfLetters > 0)) {
+            if ((setOfLetters & OtherVowels) == 0) {
                 flag = 1;
             }
-            set_of_letters = 0;
+            setOfLetters = 0;
         } 
         if (symb == WEOF) {
             break;
