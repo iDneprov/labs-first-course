979d98ba4002ef7a5315f419c71b13b287f83798
chavapchacha <lizahlebnickova@yandex.ru>
2022-12-02 11:59:34 +0300

    apdate lab14.c

--- lab14.c
+++ lab14.c
@@ -5,41 +5,41 @@
 #define MXN 7
 
 int main() {
-    int n, current_rosition, flag;
+    int n, currentPosition, flag;
     int m[MXN * MXN];
     scanf("%i", &n);
     for (int i = 0; i < (n * n); ++i) {
         scanf("%d", &m[i]);
     }
     flag = UP;
-    current_rosition = n * n - 1;
-    printf("\n%d ", m[current_rosition]);
+    currentPosition = n * n - 1;
+    printf("\n%d ", m[currentPosition]);
     for (int i = 0; i < (n * n - 1); ++i) {
         if (flag == UP) {
-            if (current_rosition / n == 0) {
-                current_rosition -= 1;
+            if (currentPosition / n == 0) {
+                currentPosition -= 1;
                 flag = DOWN; 
-            } else if (current_rosition % n == (n - 1)) {
-                current_rosition -= n;
+            } else if (currentPosition % n == (n - 1)) {
+                currentPosition -= n;
                 flag = DOWN; 
             } else {
-                current_rosition += 1;
-                current_rosition -= n;
+                currentPosition += 1;
+                currentPosition -= n;
 
             }
         } else {
-            if (current_rosition % n == 0) {
-                current_rosition -= n;
+            if (currentPosition % n == 0) {
+                currentPosition -= n;
                 flag = UP;
-            } else if (current_rosition / n == (n - 1)) {
-                current_rosition -= 1;
+            } else if (currentPosition / n == (n - 1)) {
+                currentPosition -= 1;
                 flag = UP;
             } else {
-                current_rosition += n;
-                current_rosition -= 1;
+                currentPosition += n;
+                currentPosition -= 1;
             }
         }   
-        printf("%d ", m[current_rosition]);
+        printf("%d ", m[currentPosition]);
     }
 }
 
