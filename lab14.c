833ce26b7c807fbbdf6878572d00289ebce33c06
chavapchacha <lizahlebnickova@yandex.ru>
2022-12-02 11:57:11 +0300

    apdate lab14.c

--- lab14.c
+++ lab14.c
@@ -2,8 +2,7 @@
 
 #define UP 1
 #define DOWN -1
-
-const int MXN = 100;
+#define MXN 7
 
 int main() {
     int n, current_rosition, flag;
@@ -13,14 +12,14 @@ int main() {
         scanf("%d", &m[i]);
     }
     flag = UP;
-    current_rosition = n*n-1;
+    current_rosition = n * n - 1;
     printf("\n%d ", m[current_rosition]);
     for (int i = 0; i < (n * n - 1); ++i) {
         if (flag == UP) {
             if (current_rosition / n == 0) {
                 current_rosition -= 1;
                 flag = DOWN; 
-            } else if (current_rosition % n == (n-1)) {
+            } else if (current_rosition % n == (n - 1)) {
                 current_rosition -= n;
                 flag = DOWN; 
             } else {
@@ -32,8 +31,8 @@ int main() {
             if (current_rosition % n == 0) {
                 current_rosition -= n;
                 flag = UP;
-            } else if (current_rosition / n == (n-1)) {
-                current_rosition -=1;
+            } else if (current_rosition / n == (n - 1)) {
+                current_rosition -= 1;
                 flag = UP;
             } else {
                 current_rosition += n;
@@ -43,3 +42,5 @@ int main() {
         printf("%d ", m[current_rosition]);
     }
 }
+
+
