640d74367f7f965333ab1e106ab47b776e48cad2
Bespalov Artem <75415627+BespalovArtem@users.noreply.github.com>
2022-12-03 14:34:48 +0400

    update lab13
    lab13

--- lab13.c
+++ lab13.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
+#define CONS (1ULL << ('p' - 'a') | 1ULL << ('t' - 'a') | 1ULL << ('k' - 'a') | 1ULL << ('f' - 'a') | 1ULL << ('s' - 'a'))
 
 int Check(char s) {
     return ((s != EOF) && (s != '\n') && (s != '.') && (s != ',') && (s != ' '));
 }
 
-int LetterCheck(char s) {
-    return ((s == 'p') || (s == 't') || (s == 'k') || (s == 'f') || (s == 's'));
+unsigned long long int CharToSet(char c) {
+    return  (c < 'a' || c > 'z') ? 0 : 1ULL << (c - 'a');
 }
 
 int main(void) {
-    int f = 1, flag = 0;
+    int flag = 0;
     char s;
+    unsigned long long int letters = 0;
     s = getchar();
     while (1) {
         if (Check(s)) {
-            if (LetterCheck(s)) {
-                f = 0;
-            }
+            letters = letters | CharToSet(s);
+            letters = letters & CONS;
         }
-        if (Check(s) == 0 && s != EOF) {
-            if (f == 1) {
+        if (Check(s) == 0) {
+            if (!letters) {
                 flag = 1;
-                break;
-            } else {
-                f = 1;
+            }  
+            if (letters != 0) {
+                flag = 0;
             }
+            letters = 0;
         }
-        if (s == EOF) {
-            break;
+        if (s == EOF || s == '\n') {
+            if (flag) {
+                printf("Yes\n");
+            }
+            else {
+                printf("No\n");
+            }
         }
         s = getchar();
     }
-    if (flag) {
-        printf("Yes\n");
-    } else {
-        printf("No\n");
-    }
-    return 0;
 }
