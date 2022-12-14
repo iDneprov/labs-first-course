401ac09816c6ae285014b121249c71740dd2c3c9
F0R3D0 <stephen.foredo@gmail.com>
2022-12-14 18:55:46 +0300

    lab12

--- lab12.c
+++ lab12.c
@@ -1,35 +1,21 @@
-#include <stdio.h>
- 
-int main() {
-    int k = 0, c, d = 0, e = 0;
-    double a[100] = {0};
-    while (1) {
-        c = getchar();
-        if (c == EOF || c == '\n') {
-            break;
-        }
-        if (!(c > 47 && c < 58)) {
-            if (k != 0) {
-                a[e++] = k;
-            }
-            k = 0;
-            continue;
-        }
-        k = k * 10 + (c - '0');
-        if ((c - '0') == 0) {
-            d += 1;
-        }
-    }
- 
-    for (int i = 0; i < 100; i++) {
-        int elem = a[i];
-        if (a[i] != 0) {
-            printf("%d", elem);
-            for (int j = 0; j < d; j++) {
-                printf("%d", 0);
-            }
-        }
-    }
- 
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+  int a = 10, k = 0, maxn = 214748364, minn = -214748364, n;
+  scanf("%d", &n);
+  bool f = true;
+  int s = n;
+  while (n != 0){
+    n = n / 10;
+    k++;
+  }
+  while (k < a){
+    if (s >= minn && s <= maxn) s = s * 10;
+    else f = !f;
+    k++;
+  }
+  if (f) printf("number = %d\n", s);
+  else printf("Вышел за рамки\n");
+  return 0;
 }
- 
