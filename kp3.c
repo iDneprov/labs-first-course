0975470132c230362f6e6a8bf75a8bd610a0f3ed
Lenina-Paket <115350805+Lenina-Paket@users.noreply.github.com>
2022-12-17 15:52:07 +0300

    Update kp3.c

--- kp3.c
+++ kp3.c
@@ -2,8 +2,8 @@
 #include <math.h>
 
 #define RANGE_BEGIN 0.0l
-#define RANGE_END 0.5l
-#define STEP 0.005l
+#define RANGE_END 2.0l
+#define STEP 0.25l
 
 long double MachineEpsilon() {
     long double e = 1.0l;
@@ -14,29 +14,30 @@ long double MachineEpsilon() {
 }
 
 long double F(long double x) {
-    return (x * (3 - x)) / powl((1 - x), 3);
+    return 1 / (2*x - 5);
+
 }
 
 int main(void) {
     long double e = MachineEpsilon(), x, part;
     printf("Machine epsilon for long double = %.8Le\n", e);
     printf("Table of values of Taylor series and standard function for x*(3 - x) / ((1 - x)^ 3)\n");
-        printf("------------------------------------------------------------------\n");
-	    printf("| x \t| sum of line\t\t | function\t\t  | iter |\n");
-	    printf("--------+------------------------+------------------------+-------\n");
+        printf("-------------------------------------------------\n");
+	    printf("| x \t| sum of line\t | function\t | iter |\n");
+	    printf("--------+----------------+---------------+-------\n");
     for (long double i = RANGE_BEGIN; i <= RANGE_END; i += STEP) {
         int n = 1;
         long double sum = 0.0;
         x = i;
         for (; n <= 100; n++) {
-            part = n * (n + 2) * powl(x, n);
-            sum += part;
+            part = powl(2, (n - 1)) * powl(x, (n - 1)) / powl(5, n);
+            sum -= part;
             if (part < e) {
                 break;
             }  
         }
-        printf("| %.3Lf | %.20Lf | %.20Lf | %d \t |\n", x, sum, F(x), n);
+        printf("| %.3Lf | %.10Lf  | %.10Lf | %d \t|\n", x, sum, F(x), n);
     }
-    printf("------------------------------------------------------------------\n");
+    printf("-------------------------------------------------\n");
     return 0;
 }
