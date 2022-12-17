8a762a0bf0032e3eb6c6c38d735c4281d30fc52e
sdvaanyaa <113551181+sdvaanyaa@users.noreply.github.com>
2022-12-17 16:01:23 +0300

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-int main()
-{
+int main(){
+
     long INT_MAX = 2147483647;
-    int sost=0,c;
+    int sost = 0, c;
     long n;
 
-    while(sost != 3){
+    while (sost != 3){
         c = getchar();
         switch(sost){
             case 0:
                 if ((c != ' ' && c != '\t' && c != ',' && c != '.') && c >= '0' && c <= '9'){n = (int)(c-'0'); sost = 1;}
-                else if(c == '-'){sost = 2;}
-                if(c == EOF){sost = 3;}
+                else if (c == '-'){sost = 2;}
+                if (c == EOF){sost = 3;}
                 break;
             case 1:
-                if((c != ' ' && c != '\t' && c != ','&& c != '.') && c >= '0' && c <= '9'){n = n * 10 + (int)(c-'0');}
-                else{if(n > INT_MAX){printf("%ld\n", n);}n = 0;sost = 0;}
+                if ((c != ' ' && c != '\t' && c != ','&& c != '.') && c >= '0' && c <= '9'){n = n * 10 + (int)(c-'0');}
+                else {if(n > INT_MAX){printf("%ld\n", n);}n = 0;sost = 0;}
                 break;
             case 2:
-                if((c != ' ' && c != '\t' && c != ','&& c != '.') && c >= '0' && c <= '9'){n = n * 10 + (int)(c-'0');}
-                else{if(n > INT_MAX + 1){printf("%ld\n", -n);}n = 0;sost = 0;}
+                if ((c != ' ' && c != '\t' && c != ','&& c != '.') && c >= '0' && c <= '9'){n = n * 10 + (int)(c-'0');}
+                else {if(n > INT_MAX + 1){printf("%ld\n", -n);}n = 0;sost = 0;}
                 break;
         }
     }
